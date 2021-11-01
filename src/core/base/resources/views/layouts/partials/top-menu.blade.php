8c28ab201a4e68311709d0b11c6266a79bdde269
harryoppa <harryoppa123@gmail.com>
2021-11-01 08:59:38 +0700

    Refactor code

--- src/core/base/resources/views/layouts/partials/top-menu.blade.php
+++ src/core/base/resources/views/layouts/partials/top-menu.blade.php
@@ -1,7 +1,7 @@
 <div class="top-menu">
     <ul class="nav navbar-nav float-start">
         <li class="dropdown p-0">
-            <a id="sidebar-toggler" class="dropdown-toggle dropdown-header-name" style="padding-right: 15px; padding-left: 20px;" href="javascript:;">
+            <a id="sidebar-toggler" class="dropdown-toggle dropdown-header-name sidebar-toggler" style="padding-right: 15px; padding-left: 20px;" href="javascript:;">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                 </svg>
