fe9b06d28143a266928e249f465ab704e7aec98b
Harry Tran <harry@dnkinno.com>
2022-11-29 15:27:16 +0700

    Update UI

--- src/core/setting/resources/views/index.blade.php
+++ src/core/setting/resources/views/index.blade.php
@@ -1,12 +1,5 @@
 @extends(BaseHelper::getAdminMasterLayoutTemplate())
 @section('content')
-    <div id="main-settings">
-        <license-component
-            verify-url="{{ route('settings.license.verify') }}"
-            activate-license-url="{{ route('settings.license.activate') }}"
-            deactivate-license-url="{{ route('settings.license.deactivate') }}"
-        ></license-component>
-    </div>
     {!! Form::open(['route' => ['settings.edit']]) !!}
         <div class="max-width-1200">
             <div class="flexbox-annotated-section">
